1c4e2ede42db8f2380131e4b1e1cd26b188f3576
nsafai <nicolaisafai@gmail.com>
2015-07-27 17:57:46 -0400

    Successfully tested camera integration with PBJCamera

---
+++ TicketBash/TicketBash-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  TicketBash-Bridging-Header.h
+//  TicketBash
+//
+//  Created by Nicolai Safai on 7/27/15.
+//  Copyright (c) 2015 Lime. All rights reserved.
+//
+
+#ifndef TicketBash_TicketBash_Bridging_Header_h
+#define TicketBash_TicketBash_Bridging_Header_h
+
+#import "PBJVision.h"
+
+#endif
