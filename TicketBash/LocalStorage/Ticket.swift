7d4aa8ad54572bee268bc9934e1ee72a5075203f
nsafai <nicolaisafai@gmail.com>
2015-07-28 19:25:06 -0400

    added ticket object type

---
+++ TicketBash/LocalStorage/Ticket.swift
@@ -0,0 +1,36 @@
+//
+//  Ticket.swift
+//  TicketBash
+//
+//  Created by Nicolai Safai on 7/28/15.
+//  Copyright (c) 2015 Lime. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class Ticket: Object {
+    
+    dynamic var explanationText: String = ""
+    
+    
+//    dynamic var ticketPicture: NSData!
+//    dynamic var evidencePicture: NSData!
+//    
+//    dynamic var mailingAddress: String = ""
+//    dynamic var mailingCity: String = ""
+//    dynamic var mailingZip: String = ""
+//    dynamic var mailingState: String = ""
+//    
+//    dynamic var modificationDate = NSDate()
+    
+    
+    //    User (pointer)
+    //    ticketPicture (File)
+    //    evidencePicture (File)
+    //    explanationText (string) ?
+    //    mailingAddress (string)
+    //    mailingCity (string)
+    //    mailingZip (string)
+    //    mailingState (string)
+}
