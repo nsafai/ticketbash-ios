4aceab06e34784e9c7f55eb3611fafe9fcbcdee7
nsafai <nicolaisafai@gmail.com>
2015-07-28 20:35:20 -0400

    added a realm variable and playing with saving data when changed

--- TicketBash/ViewControllers/ExplanationViewController.swift
+++ TicketBash/ViewControllers/ExplanationViewController.swift
@@ -7,17 +7,37 @@
 //
 
 import UIKit
-import Realm
+import RealmSwift
 
 class ExplanationViewController: UIViewController {
     
-    override func viewDidLoad() {
+    @IBOutlet weak var explanationTextView: UITextView!
+    
+    func viewDidLoad(animated: Bool) {
         super.viewDidLoad()
-
         
+        var ticket: Ticket? {
+            didSet {
+                if let ticket = ticket, explanationTextView = explanationTextView {
+                    self.explanationTextView.text = ticket.explanationText
+                }
+            }
+        }
         
+        let sampleTicket = Ticket()
+        sampleTicket.explanationText   = "Super Simple Test Note"
+        
+        println("excuse for sampleTicket is: \(sampleTicket.explanationText)")
+//        explanationTextView.text = "Hello"
+        
+        let realm = Realm() // 1
+        realm.write() { // 2
+            realm.add(sampleTicket) // 3
+        }
         
     }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
