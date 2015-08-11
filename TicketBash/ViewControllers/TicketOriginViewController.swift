c361e26d3ff56dcab47d04d5d893f8d2261f1be8
nsafai <nicolaisafai@gmail.com>
2015-08-10 23:21:06 -0400

    added automatic user creation and removed login view controller code

--- TicketBash/ViewControllers/TicketOriginViewController.swift
+++ TicketBash/ViewControllers/TicketOriginViewController.swift
@@ -23,18 +23,18 @@ class TicketOriginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.parseLoginHelper = ParseLoginHelper {[unowned self] user, error in // Initialize the ParseLoginHelper with a callback
-            
-            if let error = error {
-                ErrorHandling.defaultErrorHandler(error)
-                //                println("Error logging in \(user)")
-            } else if let user = user {
-                // code below indicates what should happen if PFLogInViewController succesfully logs in a user
-                //                println("CASE 1 Logged in user is \(user)")
-                self.dismissViewControllerAnimated(true, completion: nil)
-            }
-        }
-        
+//        self.parseLoginHelper = ParseLoginHelper {[unowned self] user, error in // Initialize the ParseLoginHelper with a callback
+//            
+//            if let error = error {
+//                ErrorHandling.defaultErrorHandler(error)
+//                //                println("Error logging in \(user)")
+//            } else if let user = user {
+//                // code below indicates what should happen if PFLogInViewController succesfully logs in a user
+//                //                println("CASE 1 Logged in user is \(user)")
+//                self.dismissViewControllerAnimated(true, completion: nil)
+//            }
+//        }
+
         var tickets = realm.objects(Ticket)
         if let ticket = tickets.first { // if there is a stored value then the 'tickets' array is not nil --> assign the value of the first ticket in the array to 'ticket'
             myTicket = ticket // assign the value of ticket to myTicket
@@ -60,30 +60,34 @@ class TicketOriginViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         self.navigationController?.navigationBarHidden = false
         
-        let user = PFUser.currentUser()
+        var user = PFUser.currentUser()
         if (user != nil) {
             // someone is logged in
             //            println("CASE 2 Logged in user is \(user)")
             //            self.performSegueWithIdentifier("showContactInfo", sender: self)
-            
+            println(PFUser.currentUser())
         } else {
-            // no one is logged in
+            // no one is logged in, create an automatic user
+            PFUser.enableAutomaticUser()
+            PFUser.currentUser()?.saveInBackgroundWithBlock({ (sucess, ErrorHandling) -> Void in
+                println(PFUser.currentUser())
+            })
             
-            self.loginViewController.fields = .UsernameAndPassword | .LogInButton | .SignUpButton | .PasswordForgotten
-            // add | .Facebook for Facebook
-            self.loginViewController.emailAsUsername = true
-            var logInLogoTitle = UILabel()
-            logInLogoTitle.text = "Ticket Bash"
-            logInLogoTitle.font = UIFont(name: "HelveticaNeue-UltraLight",
-                size: 70.0)
-            
-            self.loginViewController.logInView?.logo = logInLogoTitle
-            self.loginViewController.delegate = self.parseLoginHelper
-            self.loginViewController.signUpController?.delegate = self.parseLoginHelper
-            
-            // present login view controller modally
-            self.presentViewController(self.loginViewController, animated: true, completion: nil)
-            //            self.navigationController?.pushViewController(self.loginViewController, animated: true)
+//            self.loginViewController.fields = .UsernameAndPassword | .LogInButton | .SignUpButton | .PasswordForgotten
+//            // add | .Facebook for Facebook
+//            self.loginViewController.emailAsUsername = true
+//            var logInLogoTitle = UILabel()
+//            logInLogoTitle.text = "Ticket Bash"
+//            logInLogoTitle.font = UIFont(name: "HelveticaNeue-UltraLight",
+//                size: 70.0)
+//            
+//            self.loginViewController.logInView?.logo = logInLogoTitle
+//            self.loginViewController.delegate = self.parseLoginHelper
+//            self.loginViewController.signUpController?.delegate = self.parseLoginHelper
+//            
+//            // present login view controller modally
+//            self.presentViewController(self.loginViewController, animated: true, completion: nil)
+//                        self.navigationController?.pushViewController(self.loginViewController, animated: true)
         }
     }
     
